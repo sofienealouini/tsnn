413420554c7be609d81181f6abbc32e791c77858
Sofiene Alouini <salouini@octo.com>
2017-10-25 10:10:08 +0200

    added tests

--- tests/test_data_utils.py
+++ tests/test_data_utils.py
@@ -489,3 +489,36 @@ class TestDataUtilsFunctions(unittest.TestCase):
         # Check
         self.assertEqual(last_batch_x.shape, (2, 5, 5))
         self.assertEqual(last_batch_y.shape, (2, 3))
+
+    def test_sample_gen_rnn_should_yield_only_xbatch_if_inputs_only_set_true(self):
+
+        # Given
+        inputs = pd.DataFrame({'A': [1., 1., 1., 1., 1., 14., 20., -10., 12., 1., 3., -2.],
+                               'B': [-5., -3., -2., -1., 1., 1., 0., 10., 1., 1., -3., 0.],
+                               'C': [-20., -8., -11., -12., -14., 0., 0., 0., 0., 0., 7., -20.],
+                               'D': [-2., 3., 6., 7., 18., 1., 2., 3., 4., 5., -12., -5.],
+                               'E': [10., 0., 10., 12., 14., 10., 0., 0., 0., 0., 0., 1.]})
+
+        targets = pd.DataFrame({'B': [10., 1., 1., -3., 0., 10., 12., 14],
+                                'D': [3., 4., 5., -12., -5., -3., -2., -1.],
+                                'E': [0., 0., 0., 0., 1., 1., 1., 1.]})
+
+        expected_xbatch = np.array([[[1., -5., -20., -2., 10.],
+                                     [1., -3., -8., 3., 0.],
+                                     [1., -2., -11., 6., 10.],
+                                     [1., -1., -12., 7., 12.],
+                                     [1., 1., -14., 18., 14.]],
+                                    [[1., -3., -8., 3., 0.],
+                                     [1., -2., -11., 6., 10.],
+                                     [1., -1., -12., 7., 12.],
+                                     [1., 1., -14., 18., 14.],
+                                     [14., 1., 0., 1., 10.]]])
+
+        gen = sample_gen_rnn(inputs, targets, samples_length=5, sampling_step=1, batch_size=2, inputs_only=True)
+
+        # When
+        res = next(gen)
+
+        # Check
+        self.assertIsInstance(res, np.ndarray)
+        assert_equal(res, expected_xbatch)
