bd68d3fa136db0553c1d709eb9d109c672989af0
Sofiene Alouini <salouini@octo.com>
2017-10-26 10:15:16 +0200

    new test DeepRec

---
+++ tests/test_models/test_deeprec.py
@@ -0,0 +1,23 @@
+import unittest
+from unittest.mock import patch
+import numpy as np
+import keras.backend as K
+from tsnn.models.deeprec import DeepRecurrent
+
+
+class TestDeepRecurrentModel(unittest.TestCase):
+
+    def test_DeepRecurrent_should_have_correct_tensor_shapes(self):
+
+        # Given
+        expected_shapes = [(None, 168, 321), (None, 168, 128), (None, 64), (None, 5)]
+
+        # When
+        model = DeepRecurrent((168, 321), [128, 64], output_dim=5)
+        computed_shapes = [l.get_output_shape_at(node_index=0) for l in model.layers]
+
+        # Check
+        self.assertEqual(computed_shapes, expected_shapes)
+
+    def test_DeepRecurrent_should_call_layers_in_order(self):
+        pass
