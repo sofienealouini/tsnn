17089605c6d1501c0e0d73a45a3068ea9f2652de
Sofiene Alouini <salouini@octo.com>
2017-11-06 13:52:51 +0100

    added tests for DeepRecurrent

--- tests/test_models/test_deeprec.py
+++ tests/test_models/test_deeprec.py
@@ -4,17 +4,62 @@ from tsnn.models.deeprec import DeepRecurrent
 
 class TestDeepRecurrentModel(unittest.TestCase):
 
-    def test_DeepRecurrent_should_have_correct_tensor_shapes(self):
+    def test_DeepRecurrent_should_have_correct_tensor_shapes_with_no_attention(self):
 
         # Given
-        expected_shapes = [(None, 168, 321), (None, 168, 128), (None, 64), (None, 5)]
+        expected_shapes = [(None, 168, 321), (None, 168, 128), (None, 168, 64), (None, 32), (None, 5)]
 
         # When
-        model = DeepRecurrent((168, 321), [128, 64], output_dim=5)
+        model = DeepRecurrent((168, 321), [128, 64, 32], output_dim=5, attention_dim="")
         computed_shapes = [l.get_output_shape_at(node_index=0) for l in model.layers]
 
         # Check
         self.assertEqual(computed_shapes, expected_shapes)
 
-    def test_DeepRecurrent_should_call_layers_in_order(self):
-        pass
+    def test_DeepRecurrent_should_have_correct_tensor_shapes_with_timesteps_attention(self):
+
+        # Given
+        expected_shapes = [(None, 168, 321),
+                           (None, 168, 128),
+                           (None, 128, 168),
+                           (None, 128, 168),
+                           (None, 128, 168),
+                           (None, 168, 128),
+                           (None, 168, 64),
+                           (None, 64, 168),
+                           (None, 64, 168),
+                           (None, 64, 168),
+                           (None, 168, 64),
+                           (None, 32),
+                           (None, 32),
+                           (None, 32),
+                           (None, 5)]
+
+        # When
+        model = DeepRecurrent((168, 321), [128, 64, 32], output_dim=5, attention_dim="timesteps")
+        computed_shapes = [l.get_output_shape_at(node_index=0) for l in model.layers]
+
+        # Check
+        self.assertEqual(computed_shapes, expected_shapes)
+
+    def test_DeepRecurrent_should_have_correct_tensor_shapes_with_features_attention(self):
+
+        # Given
+        expected_shapes = [(None, 168, 321),
+                           (None, 168, 128),
+                           (None, 168, 128),
+                           (None, 168, 128),
+                           (None, 168, 64),
+                           (None, 168, 64),
+                           (None, 168, 64),
+                           (None, 32),
+                           (None, 32),
+                           (None, 32),
+                           (None, 5)]
+
+        # When
+        model = DeepRecurrent((168, 321), [128, 64, 32], output_dim=5, attention_dim="features")
+        computed_shapes = [l.get_output_shape_at(node_index=0) for l in model.layers]
+
+        # Check
+        self.assertEqual(computed_shapes, expected_shapes)
